1f87ef03f8061c3cca0811f2c3b75c4130e34cdc
AmrikSD <amrik.singh@live.co.uk>
2023-09-21 23:35:19 +0100

    tf fmt

--- infrastructure/cloudflare/main.tf
+++ infrastructure/cloudflare/main.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "www" {
-    zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
-    name    = "www"
-    value   = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.ip"]
-    type    = "A"
-    proxied = true
+  zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
+  name    = "www"
+  value   = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.ip"]
+  type    = "A"
+  proxied = true
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,3 @@
 module "cloudflare" {
-    source = "./cloudflare/"
+  source = "./cloudflare/"
 }
