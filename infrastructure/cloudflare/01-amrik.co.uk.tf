200d381824a0432cfa517d6ac637dc6f4d73b158
AmrikSD <amrik.singh@live.co.uk>
2023-09-25 03:26:28 +0100

    rename bucket

--- infrastructure/cloudflare/01-amrik.co.uk.tf
+++ infrastructure/cloudflare/01-amrik.co.uk.tf
@@ -8,6 +8,6 @@ resource "cloudflare_record" "www" {
 
 resource "cloudflare_r2_bucket" "amrik-co-uk" {
   account_id = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
-  name = "amrikcouk"
+  name = "amrik-co-uk"
   location = "WEUR"
 }
