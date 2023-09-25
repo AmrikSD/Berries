f6edb7cb1e58428969ce30cb239057ea9e8e067d
AmrikSD <amrik.singh@live.co.uk>
2023-09-25 04:23:15 +0100

    remove dns records

--- infrastructure/cloudflare/01-amrik.co.uk.tf
+++ infrastructure/cloudflare/01-amrik.co.uk.tf
@@ -1,11 +1,3 @@
-resource "cloudflare_record" "www" {
-  zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
-  name    = "www"
-  value   = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.ip"]
-  type    = "A"
-  proxied = true
-}
-
 resource "cloudflare_r2_bucket" "amrik-co-uk" {
   account_id = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
   name = "amrik-co-uk"
