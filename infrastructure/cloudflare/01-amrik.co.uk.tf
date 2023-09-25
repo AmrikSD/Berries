d2d8f3bc2b1517e5aa696fbd61de61d9806c3e0c
AmrikSD <amrik.singh@live.co.uk>
2023-09-25 04:39:55 +0100

    add rewrite for root

--- infrastructure/cloudflare/01-amrik.co.uk.tf
+++ infrastructure/cloudflare/01-amrik.co.uk.tf
@@ -3,3 +3,25 @@ resource "cloudflare_r2_bucket" "amrik-co-uk" {
   name = "amrik-co-uk"
   location = "WEUR"
 }
+
+resource "cloudflare_ruleset" "transform_url_rewrite" {
+  zone_id     = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
+  name        = "Transform Rule performing a static URL rewrite"
+  description = ""
+  kind        = "zone"
+  phase       = "http_request_transform"
+
+  rules {
+    action = "rewrite"
+    action_parameters {
+      uri {
+        path {
+          value = "/index.html"
+        }
+      }
+    }
+    expression = "((http.host eq \"amrik.co.uk\" or http.host eq \"www.amrik.co.uk\" ) and http.request.uri.path eq \"/\")"
+    description = "Rewrite / to index.html"
+    enabled = true
+  }
+}
