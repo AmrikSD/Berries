542ca5d6aa47b086a1c033a724f541b4c6d35037
AmrikSD <amrik.singh@live.co.uk>
2024-02-24 02:40:42 +0000

    Remove r2 bucket
    Signed-off-by: AmrikSD <amrik.singh@live.co.uk>

--- infrastructure/cloudflare/01-amrik.co.uk.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_r2_bucket" "amrik-co-uk" {
-  account_id = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
-  name       = "amrik-co-uk"
-  location   = "WEUR"
-}
